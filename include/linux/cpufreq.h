133616cc064180ac3c9ccb988781f9e10328d039
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-17 16:10:56 +0100

    WIP: cpufreq: Frequency invariant scheduler load-tracking support
    Implements cpufreq_scale_freq_capacity() to provide the scheduler with a
    frequency scaling correction factor for more accurate load-tracking.
    
    The factor is:
    
    	current_freq(cpu) << SCHED_CAPACITY_SHIFT / max_freq(cpu)
    
    In fact, freq_scale should be a struct cpufreq_policy data member. But
    this would require that the scheduler hot path (__update_load_avg()) would
    have to grab the cpufreq lock. This can be avoided by using per-cpu data
    initialized to SCHED_CAPACITY_SCALE for freq_scale.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I8aa1c424a33794db78c35a80c57184d992e71d2b
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -525,4 +525,7 @@ static inline void cpufreq_task_stats_exit(struct task_struct *p) {}
 static inline void cpufreq_task_stats_remove_uids(uid_t uid_start,
 	uid_t uid_end) {}
 #endif
+
+struct sched_domain;
+unsigned long cpufreq_scale_freq_capacity(struct sched_domain *sd, int cpu);
 #endif /* _LINUX_CPUFREQ_H */
