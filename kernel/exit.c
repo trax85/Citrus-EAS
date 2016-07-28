7d197ddc4f6477e3673526164083e21c75fd86c5
Patrick Bellasi <patrick.bellasi@arm.com>
2016-07-28 18:44:40 +0100

    FIXUP: sched/tune: fix accounting for runnable tasks
    Contains:
    
    sched/tune: fix accounting for runnable tasks (1/5)
    
    The accounting for tasks into boost groups of different CPUs is currently
    broken mainly because:
    a) we do not properly track the change of boost group of a RUNNABLE task
    b) there are race conditions between migration code and accounting code
    
    This patch provides a fixes to ensure enqueue/dequeue
    accounting also for throttled tasks.
    
    Without this patch is can happen that a task is enqueued into a throttled
    RQ thus not being accounted for the boosting of the corresponding RQ.
    We could argue that a throttled task should not boost a CPU, however:
    a) properly implementing CPU boosting considering throttled tasks will
       increase a lot the complexity of the solution
    b) it's not easy to quantify the benefits introduced by such a more
       complex solution
    
    Since task throttling requires the usage of the CFS bandwidth controller,
    which is not widely used on mobile systems (at least not by Android kernels
    so far), for the time being we go for the simple solution and boost also
    for throttled RQs.
    
    sched/tune: fix accounting for runnable tasks (2/5)
    
    This patch provides the code required to enforce proper locking.
    A per boost group spinlock has been added to grant atomic
    accounting of tasks as well as to serialise enqueue/dequeue operations,
    triggered by tasks migrations, with cgroups's attach/detach operations.
    
    sched/tune: fix accounting for runnable tasks (3/5)
    
    This patch adds cgroups {allow,can,cancel}_attach callbacks.
    
    Since a task can be migrated between boost groups while it's running,
    the CGroups's attach callbacks have been added to properly migrate
    boost contributions of RUNNABLE tasks.
    
    The RQ's lock is used to serialise enqueue/dequeue operations, triggered
    by tasks migrations, with cgroups's attach/detach operations. While the
    SchedTune's CPU lock is used to grant atrocity of the accounting within
    the CPU.
    
    NOTE: the current implementation does not allows a concurrent CPU migration
          and CGroups change.
    
    sched/tune: fix accounting for runnable tasks (4/5)
    
    This fixes accounting for exiting tasks by adding a dedicated call early
    in the do_exit() syscall, which disables SchedTune accounting as soon as a
    task is flagged PF_EXITING.
    
    This flag is set before the multiple dequeue/enqueue dance triggered
    by cgroup_exit() which is useful only to inject useless tasks movements
    thus increasing possibilities for race conditions with the migration code.
    The schedtune_exit_task() call does the last dequeue of a task from its
    current boost group. This is a solution more aligned with what happens in
    mainline kernels (>v4.4) where the exit_cgroup does not move anymore a dying
    task to the root control group.
    
    sched/tune: fix accounting for runnable tasks (5/5)
    
    To avoid accounting issues at startup, this patch disable the SchedTune
    accounting until the required data structures have been properly
    initialized.
    
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -55,6 +55,8 @@
 #include <linux/shm.h>
 #include <linux/cpufreq.h>
 
+#include "sched/tune.h"
+
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 #include <asm/pgtable.h>
@@ -782,6 +784,9 @@ void do_exit(long code)
 	if (tsk->flags & PF_SU) {
 		su_exit();
 	}
+	sched_exit(tsk);
+
+	schedtune_exit_task(tsk);
 
 	/*
 	 * tsk->flags are checked in the futex code to protect against
