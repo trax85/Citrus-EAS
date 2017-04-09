5507e25f9b7932cf62959cae26c3dcdb865df39e
Sultanxda <sultanxda@gmail.com>
2017-04-09 15:38:02 -0700

    Properly fix use of uninitialized variables
    While most of these were harmless, there were several dangerous instances
    of uninitialized variable usage that influenced functionality. There were
    quite a few instances of explicit uninitialized pointer dereferences, which
    can cause kernel panics.
    
    This patch is for the Nougat kernel.
    
    Change-Id: Ia6826dbc95afa25f99dc7a796aacb90ffd428c26
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    Conflicts:
    	drivers/leds/leds-qpnp.c
    	drivers/power/qcom/msm-core.c
    	drivers/pwm/pwm-qpnp.c
    	drivers/staging/qcacld-2.0/CORE/SYS/legacy/src/utils/src/parserApi.c
    	drivers/video/msm/mdss/mdss_fb.c
    
    Conflicts:
    	drivers/mmc/host/cmdq_hci.c
    	drivers/power/pmic-voter.c
    	drivers/power/qcom/msm-core.c
    	drivers/usb/gadget/function/u_ether.c
    	drivers/video/msm/mdss/mdss_fb.c
    	sound/soc/codecs/wcd9335.c
    
    Signed-off-by: Miguel Martinez <slverwolf@gmail.com>
    
    Conflicts:
    	drivers/char/diag/diag_dci.c
    	drivers/clk/msm/clock-alpha-pll.c
    	drivers/clk/msm/clock-cpu-8996.c
    	drivers/input/touchscreen/synaptics_driver_s1302.c
    	drivers/input/touchscreen/synaptics_driver_s3320.c
    	drivers/media/platform/msm/camera_v2/fd/msm_fd_dev.c
    	drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
    	drivers/media/platform/msm/camera_v2/jpeg_dma/msm_jpeg_dma_hw.c
    	drivers/media/platform/msm/vidc/hfi_response_handler.c
    	drivers/media/platform/msm/vidc/msm_vidc_common.c
    	drivers/mmc/card/block.c
    	drivers/mmc/core/bus.c
    	drivers/net/wireless/cnss/cnss_pci.c
    	drivers/platform/msm/qpnp-power-on.c
    	drivers/power/pmic-voter.c
    	drivers/power/qcom/msm-core.c
    	drivers/power/qpnp-fg.c
    	drivers/power/qpnp-smbcharger.c
    	drivers/regulator/cpr3-hmss-regulator.c
    	drivers/regulator/cpr3-regulator.c
    	drivers/regulator/kryo-regulator.c
    	drivers/soc/qcom/glink.c
    	drivers/soc/qcom/qbt1000.c
    	drivers/soc/qcom/watchdog_v2.c
    	drivers/soundwire/swr-wcd-ctrl.c
    	drivers/usb/gadget/android.c
    	drivers/usb/gadget/function/u_ether.c
    	drivers/video/msm/mdss/mdss_dsi.c
    	drivers/video/msm/mdss/mdss_fb.c
    	drivers/video/msm/mdss/mdss_hdmi_util.c
    	drivers/video/msm/mdss/mdss_mdp.c
    	drivers/video/msm/mdss/mdss_mdp_ctl.c
    	drivers/video/msm/mdss/mdss_mdp_layer.c
    	drivers/video/msm/mdss/mdss_mdp_util.c
    	drivers/video/msm/mdss/mdss_rotator.c
    	fs/f2fs/node.c
    	fs/splice.c
    	kernel/sched/core.c
    	kernel/time/posix-cpu-timers.c
    	sound/soc/codecs/wcd-mbhc-v2.c
    	sound/soc/codecs/wcd9335.c
    	sound/soc/codecs/wcd9xxx-common-v2.c
    	sound/soc/codecs/wcd9xxx-mbhc.c
    	sound/soc/soc-cache.c
    	sound/soc/soc-pcm.c

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -396,7 +396,7 @@ static int wcd_cancel_btn_work(struct wcd_mbhc *mbhc)
 
 static bool wcd_swch_level_remove(struct wcd_mbhc *mbhc)
 {
-	u16 result2;
+	u16 result2 = 0;
 
 	WCD_MBHC_REG_READ(WCD_MBHC_SWCH_LEVEL_REMOVE, result2);
 	return (result2) ? true : false;
@@ -431,7 +431,7 @@ static void wcd_cancel_hs_detect_plug(struct wcd_mbhc *mbhc,
 static void wcd_mbhc_clr_and_turnon_hph_padac(struct wcd_mbhc *mbhc)
 {
 	bool pa_turned_on = false;
-	u8 wg_time;
+	u8 wg_time = 0;
 
 	WCD_MBHC_REG_READ(WCD_MBHC_HPH_CNP_WG_TIME, wg_time);
 	wg_time += 1;
@@ -471,7 +471,7 @@ static bool wcd_mbhc_is_hph_pa_on(struct wcd_mbhc *mbhc)
 
 static void wcd_mbhc_set_and_turnoff_hph_padac(struct wcd_mbhc *mbhc)
 {
-	u8 wg_time;
+	u8 wg_time = 0;
 
 	WCD_MBHC_REG_READ(WCD_MBHC_HPH_CNP_WG_TIME, wg_time);
 	wg_time += 1;
@@ -1423,7 +1423,7 @@ static void wcd_mbhc_detect_plug_type(struct wcd_mbhc *mbhc)
 
 static void wcd_mbhc_swch_irq_handler(struct wcd_mbhc *mbhc)
 {
-	bool detection_type;
+	bool detection_type = false;
 	bool micbias1 = false;
 	struct snd_soc_codec *codec = mbhc->codec;
 
