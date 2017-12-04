3dafc06708a7cb72ca976f8ab9b4e5d9182a7f7e
Liam Mark <lmark@codeaurora.org>
2017-12-04 10:58:55 -0800

    ion: ensure CMO target is valid
    Cleanup ION cache maintenance code to properly validate the target of
    userspace cache maintenance requests.
    
    Change-Id: I55b8e3584c59634f95250bc7c0bce5d8d70e6a13
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Swetha Chikkaboraiah <schikk@codeaurora.org>

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014,2016,2018 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, 2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -1110,4 +1110,3 @@ static void __exit msm_ion_exit(void)
 
 subsys_initcall(msm_ion_init);
 module_exit(msm_ion_exit);
-
